98cf340b8113e7d32de044bec7280578007c33ff
ambuc <bucklnd2@illinois.edu>
2015-05-18 10:36:57 +0200

    'indentation' or whatever

--- fart.py
+++ fart.py
@@ -8,13 +8,13 @@ farts = ['brrt', 'braaah', 'THPPTPHTPHPHHPH', 'phhhhhhrt', 'PPPPPPPPPPPPPPPPPPPP
 counter = 10000
 while counter > 0:
 	thisFart = ""
-	thisFart += random.choice(['b','t','p','p','f','f','f','w']) 	* random.choice([1,1,1,1,2,2,2,3,5,7])
-	thisFart += random.choice(['a','a','u','o','','','','']) 		* random.choice([1,1,1,2,2,2,2,2,4])
-	thisFart += random.choice(['r','p','h','l']) 					* random.choice([1,1,1,1,2,2,2,2,3,4,5])
+	thisFart += random.choice(['b','t','p','p','f','f','f','w']) * random.choice([1,1,1,1,2,2,2,3,5,7])
+	thisFart += random.choice(['a','a','u','o','','','','']) * random.choice([1,1,1,2,2,2,2,2,4])
+	thisFart += random.choice(['r','p','h','l']) * random.choice([1,1,1,1,2,2,2,2,3,4,5])
 	thisFart += random.choice(farts)
-	thisFart += random.choice(['r','p','h','l']) 					* random.choice([1,1,1,1,1,2,2,2,3,3,4,5])
-	thisFart += random.choice(['a','a','u','o','','','','']) 		* random.choice([1,1,1,1,1,2,2,2,3,4])
-	thisFart += random.choice(['b','t','p','p','f','f','f','w']) 	* random.choice([2,3,5,7])
+	thisFart += random.choice(['r','p','h','l']) * random.choice([1,1,1,1,1,2,2,2,3,3,4,5])
+	thisFart += random.choice(['a','a','u','o','','','','']) * random.choice([1,1,1,1,1,2,2,2,3,4])
+	thisFart += random.choice(['b','t','p','p','f','f','f','w']) * random.choice([2,3,5,7])
 
 	outFart.write(thisFart)
 	if counter != 1:
